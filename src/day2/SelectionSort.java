e5664b9069036cdc9b1bffb8c2d2d6494325d0f6
Chris Kapend <chriskapend@gmail.com>
2020-09-01 11:55:28 +0200

    #Day2 Implemented selection sort which run in best and worst case in O(n)

--- src/day2/SelectionSort.java
+++ src/day2/SelectionSort.java
@@ -2,25 +2,25 @@ package day2;
 
 public class SelectionSort {
     public static void main(String [] args){
-        int [] unsorted = {5, 2, 4, 6, 1, 3, 9}, sorted = {1, 2, 4, 5, 6, 7, 8}; //input : an unsorted array
+        int [] unsorted = {5, 2, 4, 6, 1, 3, 9}; //input : an unsorted array
         readArray(unsorted); //sorting the array
-        readArray(selectionSort(unsorted)); //sort the array in decreasing order
-        readArray(selectionSort(sorted));
+        selectionSort(unsorted); //sort the array in decreasing order
+        readArray(unsorted);
     }
-    static int [] selectionSort(int []A){
+    static void selectionSort(int []A){
         for (int i =0; i < A.length; i++) {
-            int low = i;
-            int j = i + 1;
+            int low = i; // the lowest element's index at the beginning of the loop this value is equal to i which is the first element of the Array
+            int j = i + 1; // second element index the one to which the lowest element is compared to
             for (; j < A.length; j++) {
-                if(A[low] > A[j]){
+                if(A[low] > A[j]){ //if the lowest element become greater than the current jth element then set that jth element as the lowest
                     low = j;
                 }
             }
+            //at the end of the inner loop the we've found the lowest element in the sub array [i . . . Array.length] we then swap it. The sub array [0 . . . i] is always sorted
             int swap = A[i];
             A[i] = A[low];
             A[low] = swap;
         }
-        return A;
     }
     static void readArray(int [] array){
         for (int i = 0; i < array.length; i++) {
